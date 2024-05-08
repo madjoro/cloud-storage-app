e16d75badf7e169efd9f6de05d6b0a87af03e749
madjo7 <markodjo7@gmail.com>
2024-05-08 13:42:19 +0200

    initial commit, configured bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BucketListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cloud-storage-app';
---
+++ src/app/bucket-list/bucket-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './bucket-list.component.html',
+  styleUrl: './bucket-list.component.css'
+})
+export class BucketListComponent {
+
+}
