89f123f2fe8975a5f25caf4a4a34c16ae79eca9b
madjo7 <markodjo7@gmail.com>
2024-05-08 16:43:35 +0200

    added server

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+let buckets = require("./data.json");
+
+// endpoints --------------------------------------
+app.get("/buckets", (req, res) => {
+  res.json(buckets);
+});
+
+app.post("/buckets", (req, res) => {
+  const newBucket = req.body;
+  buckets.push(newBucket);
+  saveDataToFile(buckets);
+  res.json({ message: "Bucket added successfully" });
+});
+
+app.put("/buckets/:id", (req, res) => {
+  const bucketId = req.params.id;
+  const updatedBucket = req.body;
+  const index = buckets.findIndex((bucket) => bucket.id == bucketId);
+  if (index !== -1) {
+    buckets[index] = updatedBucket;
+    saveDataToFile(buckets);
+    res.json({ message: "Bucket updated successfully" });
+  } else {
+    res.status(404).json({ message: "Bucket not found" });
+  }
+});
+
+app.delete("/buckets/:id", (req, res) => {
+  const bucketId = req.params.id;
+  const index = buckets.findIndex((bucket) => bucket.id == bucketId);
+  if (index !== -1) {
+    buckets.splice(index, 1);
+    saveDataToFile(buckets);
+    res.json({ message: "Bucket deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Bucket not found" });
+  }
+});
+
+// dump data to file
+function saveDataToFile(data) {
+  fs.writeFileSync("./src/data/buckets.json", JSON.stringify(data, null, 2));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/app/bucket-list/bucket-list.component.ts
+++ src/app/bucket-list/bucket-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bucket-list',
   standalone: true,
   imports: [],
   templateUrl: './bucket-list.component.html',
-  styleUrl: './bucket-list.component.css'
+  styleUrl: './bucket-list.component.css',
 })
-export class BucketListComponent {
+export class BucketListComponent implements OnInit {
+  buckets: any[] = [];
+  selectedBucket: any;
+  createExpanded: boolean = false;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('src/data/buckets.json').subscribe((data) => {
+      this.buckets = data;
+    });
+  }
+
+  showFiles(bucket: any) {
+    this.selectedBucket = bucket;
+  }
+
+  expandCreateBucket() {
+    this.createExpanded = !this.createExpanded;
+  }
 }
